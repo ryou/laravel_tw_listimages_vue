721a3f2caa57e0e3cb9c233a0d0f88886edee124
admin <admin@admin-no-iMac.local>
2017-04-19 21:34:20 +0900

    レイアウト調整

--- config/ttwitter.php
+++ config/ttwitter.php
@@ -5,16 +5,16 @@
 return [
 	'debug'               => function_exists('env') ? env('APP_DEBUG', false) : false,
 
-	// 'API_URL'             => 'api.twitter.com',
-	'API_URL'             => '192.168.11.5:8000',
+	'API_URL'             => 'api.twitter.com',
+	// 'API_URL'             => '192.168.11.5:8000',
 	'UPLOAD_URL'          => 'upload.twitter.com',
 	'API_VERSION'         => '1.1',
 	'AUTHENTICATE_URL'    => 'https://api.twitter.com/oauth/authenticate',
 	'AUTHORIZE_URL'       => 'https://api.twitter.com/oauth/authorize',
 	'ACCESS_TOKEN_URL'    => 'https://api.twitter.com/oauth/access_token',
 	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
-	// 'USE_SSL'             => true,
-    'USE_SSL'             => false,
+	'USE_SSL'             => true,
+    // 'USE_SSL'             => false,
 
 	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '') : '',
 	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', '') : '',
