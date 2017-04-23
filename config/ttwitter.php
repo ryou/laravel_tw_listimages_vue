fb29ffe005223e80598297f0a983a051549feae0
Your Name <you@example.com>
2017-04-23 22:32:40 +0900

    インデントをタブからスペースに修正

--- config/ttwitter.php
+++ config/ttwitter.php
@@ -3,9 +3,9 @@
 // You can find the keys here : https://apps.twitter.com/
 
 return [
-	'debug'               => function_exists('env') ? env('APP_DEBUG', false) : false,
+    'debug'               => function_exists('env') ? env('APP_DEBUG', false) : false,
 
-	'API_URL'             => call_user_func(function() {
+    'API_URL'             => call_user_func(function() {
         $defaultUrl = 'api.twitter.com';
 
         if (function_exists('env') && env('USE_MOCK', false))
@@ -15,16 +15,16 @@ return [
 
         return $defaultUrl;
     }),
-	'UPLOAD_URL'          => 'upload.twitter.com',
-	'API_VERSION'         => '1.1',
-	'AUTHENTICATE_URL'    => 'https://api.twitter.com/oauth/authenticate',
-	'AUTHORIZE_URL'       => 'https://api.twitter.com/oauth/authorize',
-	'ACCESS_TOKEN_URL'    => 'https://api.twitter.com/oauth/access_token',
-	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
-	'USE_SSL'             => (function_exists('env') && env('USE_MOCK', false)) ? false : true,
+    'UPLOAD_URL'          => 'upload.twitter.com',
+    'API_VERSION'         => '1.1',
+    'AUTHENTICATE_URL'    => 'https://api.twitter.com/oauth/authenticate',
+    'AUTHORIZE_URL'       => 'https://api.twitter.com/oauth/authorize',
+    'ACCESS_TOKEN_URL'    => 'https://api.twitter.com/oauth/access_token',
+    'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
+    'USE_SSL'             => (function_exists('env') && env('USE_MOCK', false)) ? false : true,
 
-	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '') : '',
-	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', '') : '',
-	'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '') : '',
-	'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', '') : '',
+    'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '') : '',
+    'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', '') : '',
+    'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '') : '',
+    'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', '') : '',
 ];
